a8d9c5e39d3a4e217c66167448a05ba22adeb047
Jarnotk <jarnotk@gmail.com>
2026-01-07 21:59:35 +0100

    Added code to layout.css

--- js/logic.js
+++ js/logic.js
@@ -1,8 +1,22 @@
+// Een variabel is een container waar een waarde in bewaard kan worden zodat deze later opnieuw
+// gebruikt kan worden.
+let question = "Was is de hoofdstad van Nederland?"
+let answer = "Amsterdam"
+
+
 const cardContent = document.getElementById("card-content");
 const flipButton = document.getElementById("flip-button");
 
-function changeContent() {
-    cardContent.innerText = "Antwoord is 4!"
+let flipped = true;
+
+function flipCard() {
+    if (flipped == true) {
+        cardContent.innerText = answer;
+        flipped = false;
+    } else {
+        cardContent.innerText = question;
+        flipped = true;
+    }
 }
 
-flipButton.addEventListener("click", changeContent);
+flipButton.addEventListener("click", flipCard);
